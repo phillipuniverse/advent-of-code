a9a91b00ed3884fb2d86006521f4bc11fa44a3a1
Phillip Verheyden <pverheyden@gmail.com>
2018-12-05 00:41:49 -0600

    2018 Day 5 part 1 trim the trailing newline!

--- 2018/day05/solve.js
+++ 2018/day05/solve.js
@@ -2,7 +2,7 @@ import { part1, part2 } from './solution.js'
 
 var fs = require('fs');
 
-let input = fs.readFileSync('input', 'utf8');
+let input = fs.readFileSync('input', 'utf8').trim();
 console.log("Initial size: " + input.length);
 console.log("Part 1: " + part1(input) + " with size " + part1(input).length);
 // console.log("Part 2: " + part2(input));
