3f3e3ca485f7a55fb18c6317741ba38f22d4b70c
Phillip Verheyden <pverheyden@gmail.com>
2018-12-11 19:15:30 -0600

    2018 Day 11 Part 1 - Deal with multiple stars per position

--- 2018/day10/solution.js
+++ 2018/day10/solution.js
@@ -44,71 +44,76 @@ export const initializeSky = input => {
     y: Math.abs(topBounds)
   }
   console.log(`Perspective: ${JSON.stringify(perspective)}`)
-  let sky = new Array(width)
+  let sky = {
+    positions: new Array(width)
         .fill()
-        .map((e, i) => new Array(height).fill())
-  sky[perspective.x][perspective.y] = perspective
+        // fill it with an empty array since more than 1 point can exist in a
+        // position
+        .map((e, i) => new Array(height).fill()),
+    perspective: perspective
+  }
   // console.log(visualizeSky(sky))
 
   points.forEach(p => {
-    place(p, sky, perspective)
+    place(p, sky, sky.perspective)
   })
   // console.log(visualizeSky(sky))
   return sky
 }
 
 export const move = (sky, tick) => {
-  let perspective = findPerspective(sky)
-  for (let x = 0; x < sky.length; x++) {
-    for (let y = 0; y < sky.length; y++) {
-      let point = sky[x][y]
-      while (point && !isPerspective(point) && point.tick != tick) {
-        // console.log(`Moving point at position ${JSON.stringify(point.position)} by velocity ${JSON.stringify(point.velocity)}`)
+  let pointsMoved = 0
+  for (let x = 0; x < sky.positions.length; x++) {
+    for (let y = 0; y < sky.positions[y].length; y++) {
+      let points = sky.positions[x][y] ? sky.positions[x][y] : []
+      let pointsToMove = points.length
+      for (let i = 0; i < pointsToMove; i++) {
+        let point = points[points.length-1]
+        // if I've already moved this for this interval, don't move again
+        if (point.tick == tick) { continue }
+        points.pop()
+        point.tick++
+        console.log(`Moving point ${JSON.stringify(point)}`)
+        // update the positioning
         point.position = {
           x: point.position.x + point.velocity.x,
           y: point.position.y + point.velocity.y
         }
-        point.tick++
-        // console.log(`New relative position: ${JSON.stringify(point.position)}`)
-        // clear out  the current point
-        sky[x][y] = null
-        point = place(point, sky, perspective)
+        console.log(`New relative position: ${JSON.stringify(point.position)}`)
+        // place the point
+        place(point, sky)
+        pointsMoved++
       }
     }
   }
+  console.log(`Points moved: ${pointsMoved}`)
   return sky
-}
-
-export const findPerspective = sky => {
-  for (let x = 0; x < sky.length; x++) {
-    for (let y = 0; y < sky.length; y++) {
-      if (isPerspective(sky[x][y])) { return sky[x][y] }
-    }
-  }
 }
 
 /**
  * Puts a point in the sky relative to the perspective. Returns the point that was removed
  * as a result
  */
-export const place = (point, sky, perspective) => {
-  let replacing = sky[perspective.x + point.position.x][perspective.y + point.position.y]
-  sky[perspective.x + point.position.x][perspective.y + point.position.y] = point
-  return replacing
+export const place = (point, sky) => {
+  console.log(`Plotting point at: ${JSON.stringify(point.position)}`)
+  let coords = {
+    x: sky.perspective.x + point.position.x,
+    y: sky.perspective.y + point.position.y
+  }
+  if (!sky.positions[coords.x][coords.y]) { sky.positions[coords.x][coords.y] = [] }
+  sky.positions[coords.x][coords.y].push(point)
 }
 
 export const visualizeSky = sky => {
   let out = ''
+  let map = sky.positions
   // this is a square, so assume the y is the same for everything
-  for (let y = 0; y < sky[0].length; y++) {
-    for (let x = 0; x < sky.length; x++) {
-      let point = sky[x][y]
-      if (point) {
-        if (isPerspective(point)) {
-          out += 'P'
-        } else {
-          out += '#'
-        }
+  for (let y = 0; y < map[0].length; y++) {
+    for (let x = 0; x < map.length; x++) {
+      let points = map[x][y]
+      if (points && points.length) {
+        // maybe differentiate the perspective here?
+        out += '#'
       } else {
         // empty space
         out += '.'
@@ -117,10 +122,6 @@ export const visualizeSky = sky => {
     out += '\n'
   }
   return out.trim()
-}
-
-export const isPerspective = point => {
-  return point && !point.velocity
 }
 
 export const parse = line => {
