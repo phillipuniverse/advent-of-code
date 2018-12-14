5416a2bdc245c79d8cf8c74316ddd3c8a8994120
Phillip Verheyden <pverheyden@gmail.com>
2018-12-13 19:26:12 -0600

    Day 10 Part 1 - convert to an in-memory representation of the sky
    Rather than try to compute each time, this outputs the string at the very end.
    
    Turns out that the area always shrinks. Once it gets bigger, the iteration before is the message

--- 2018/day10/solution.js
+++ 2018/day10/solution.js
@@ -1,118 +1,58 @@
 const _ = require('lodash');
 
 export const part1 = input => {
-  let sky = initializeSky(input)
-  console.log(`Sky at tick 0`)
-  console.log(visualizeSky(sky))
-  let tick = 1
-  console.log(`Type any key to see the sky at second ${1}, ctrl+c to exit`)
+  let points = input.map(parse)
 
-  const cli = process.stdin
-  cli.setEncoding('utf-8')
-  // wait for input
-  // cli.on('data', (data) => {
-  //   console.log(`Received some data ${data}`)
-  //   move(sky, tick)
-  //   console.log('\n')
-  //   console.log(`Sky at tick ${tick}`)
-  //   console.log(visualizeSky(sky))
-  //   tick++
-  // })
-  while (tick <= 5) {
-    move(sky, tick)
-    console.log('\n')
-    console.log(`Sky at tick ${tick}`)
-    console.log(visualizeSky(sky))
+  let tick = 0
+  let previousPoints = []
+  while (true) {
+    let bounds = computeBounds(points)
+    let newPoints = points.map(move)
+    let newBounds = computeBounds(newPoints)
+    if (newBounds.area >= bounds.area) {
+      console.log(`Found at tick ${tick}`)
+      let message = visualize(points, bounds)
+      console.log(`\n\n${message}\n\n`)
+      return message
+    }
+    previousPoints.push(points)
+    points = newPoints
     tick++
   }
-  // stick around for the user
-  // while(true) { }
 }
 
-export const initializeSky = input => {
-  let points = input.map(parse)
-  let leftBounds = _.minBy(points, p => p.position.x).position.x
-  let rightBounds = _.maxBy(points, p => p.position.x).position.x
-  // add 1 to consider myself
-  let width = Math.abs(leftBounds) + rightBounds + 1
-  let topBounds = _.minBy(points, p => p.position.y).position.y
-  let bottomBounds = _.maxBy(points, p => p.position.y).position.y
-  let height = Math.abs(topBounds) + bottomBounds + 1
-
-  let perspective = {
-    x: Math.abs(leftBounds),
-    y: Math.abs(topBounds)
+export const computeBounds = points => {
+  let bounds = {
+    left: _.minBy(points, p => p.position.x).position.x,
+    right: _.maxBy(points, p => p.position.x).position.x,
+    top: _.minBy(points, p => p.position.y).position.y,
+    bottom: _.maxBy(points, p => p.position.y).position.y
   }
-  console.log(`Perspective: ${JSON.stringify(perspective)}`)
-  let sky = {
-    positions: new Array(width)
-        .fill()
-        // fill it with an empty array since more than 1 point can exist in a
-        // position
-        .map((e, i) => new Array(height).fill()),
-    perspective: perspective
-  }
-  // console.log(visualizeSky(sky))
-
-  points.forEach(p => {
-    place(p, sky, sky.perspective)
-  })
-  // console.log(visualizeSky(sky))
-  return sky
+  let width = Math.abs(bounds.left) + bounds.right
+  let height = Math.abs(bounds.top) + bounds.bottom
+  bounds.area = width * height
+  return bounds
 }
 
-export const move = (sky, tick) => {
-  let pointsMoved = 0
-  for (let x = 0; x < sky.positions.length; x++) {
-    for (let y = 0; y < sky.positions[y].length; y++) {
-      let points = sky.positions[x][y] ? sky.positions[x][y] : []
-      let pointsToMove = points.length
-      for (let i = 0; i < pointsToMove; i++) {
-        let point = points[points.length-1]
-        // if I've already moved this for this interval, don't move again
-        if (point.tick == tick) { continue }
-        points.pop()
-        point.tick++
-        console.log(`Moving point ${JSON.stringify(point)}`)
-        // update the positioning
-        point.position = {
-          x: point.position.x + point.velocity.x,
-          y: point.position.y + point.velocity.y
-        }
-        console.log(`New relative position: ${JSON.stringify(point.position)}`)
-        // place the point
-        place(point, sky)
-        pointsMoved++
-      }
+export const move = point => {
+  return {
+    position: {
+      x: point.position.x + point.velocity.x,
+      y: point.position.y + point.velocity.y
+    },
+    velocity: {
+      x: point.velocity.x,
+      y: point.velocity.y
     }
   }
-  console.log(`Points moved: ${pointsMoved}`)
-  return sky
 }
 
-/**
- * Puts a point in the sky relative to the perspective. Returns the point that was removed
- * as a result
- */
-export const place = (point, sky) => {
-  console.log(`Plotting point at: ${JSON.stringify(point.position)}`)
-  let coords = {
-    x: sky.perspective.x + point.position.x,
-    y: sky.perspective.y + point.position.y
-  }
-  if (!sky.positions[coords.x][coords.y]) { sky.positions[coords.x][coords.y] = [] }
-  sky.positions[coords.x][coords.y].push(point)
-}
-
-export const visualizeSky = sky => {
+export const visualize = (points, bounds) => {
   let out = ''
-  let map = sky.positions
-  // this is a square, so assume the y is the same for everything
-  for (let y = 0; y < map[0].length; y++) {
-    for (let x = 0; x < map.length; x++) {
-      let points = map[x][y]
-      if (points && points.length) {
-        // maybe differentiate the perspective here?
+  for (let y = bounds.top; y <= bounds.bottom; y++) {
+    for (let x = bounds.left; x <= bounds.right; x++) {
+      if (points.find(p => p.position.x == x && p.position.y == y)) {
+        // found me a point
         out += '#'
       } else {
         // empty space
