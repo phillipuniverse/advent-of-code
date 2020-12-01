96f57368000053cb4898ca90a927a2f00a8a0529
Phillip Verheyden <pverheyden@gmail.com>
2020-12-01 08:30:29 -0600

    Better types by converting the input to integers upfront

--- 2020/src/day01.py
+++ 2020/src/day01.py
@@ -1,27 +1,26 @@
 from os import path
-from typing import AnyStr
 
 
-def solve_part_1(numbers: list[AnyStr], sum_equality_check: int) -> tuple[int, int]:
-    for first in numbers:
-        for second in numbers:
-            if int(first) + int(second) == sum_equality_check:
-                return int(first), int(second)
+def solve_part_1(input_numbers: list[int], sum_equality_check: int) -> tuple[int, int]:
+    for first in input_numbers:
+        for second in input_numbers:
+            if first + second == sum_equality_check:
+                return first, second
 
 
-def solve_part_2(numbers: list[AnyStr], sum_equality_check: int) -> tuple[int, int, int]:
-    for first in numbers:
-        for second in numbers:
-            for third in numbers:
-                if int(first) + int(second) + int(third) == sum_equality_check:
-                    return int(first), int(second), int(third)
+def solve_part_2(input_numbers: list[int], sum_equality_check: int) -> tuple[int, int, int]:
+    for first in input_numbers:
+        for second in input_numbers:
+            for third in input_numbers:
+                if first + second + third == sum_equality_check:
+                    return first, second, third
 
 
 if __name__ == '__main__':
     with open(path.join(path.dirname(__file__), 'input/day01-1')) as data:
-        lines = data.readlines()
-        part1_pieces = solve_part_1(lines, 2020)
+        numbers = list(map(int, data.readlines()))
+        part1_pieces = solve_part_1(numbers, 2020)
         print(f"Day 1 part1 answer: {part1_pieces[0]}*{part1_pieces[1]} = {part1_pieces[0]*part1_pieces[1]}")
 
-        part2_pieces = solve_part_2(lines, 2020)
+        part2_pieces = solve_part_2(numbers, 2020)
         print(f"Day 1 part2 answer: {part2_pieces[0]}*{part2_pieces[1]}*{part2_pieces[2]} = {part2_pieces[0]*part2_pieces[1]*part2_pieces[2]}")
