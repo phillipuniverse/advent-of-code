16b52ef9fb9e6378b9e1c3a915ed3bcc0edfdc25
Phillip Verheyden <pverheyden@gmail.com>
2020-12-01 08:16:40 -0600

    Naive way to solve day 2

--- 2020/src/day01.py
+++ 2020/src/day01.py
@@ -9,7 +9,19 @@ def solve_part_1(numbers: list[AnyStr], sum_equality_check: int) -> tuple[int, i
                 return int(first), int(second)
 
 
+def solve_part_2(numbers: list[AnyStr], sum_equality_check: int) -> tuple[int, int, int]:
+    for first in numbers:
+        for second in numbers:
+            for third in numbers:
+                if int(first) + int(second) + int(third) == sum_equality_check:
+                    return int(first), int(second), int(third)
+
+
 if __name__ == '__main__':
     with open(path.join(path.dirname(__file__), 'input/day01-1')) as data:
-        parts = solve_part_1(data.readlines(), 2020)
-        print(f"Day 1 answer: {parts[0]}*{parts[1]} = {parts[0]*parts[1]}")
+        lines = data.readlines()
+        part1_pieces = solve_part_1(lines, 2020)
+        print(f"Day 1 part1 answer: {part1_pieces[0]}*{part1_pieces[1]} = {part1_pieces[0]*part1_pieces[1]}")
+
+        part2_pieces = solve_part_2(lines, 2020)
+        print(f"Day 1 part2 answer: {part2_pieces[0]}*{part2_pieces[1]}*{part2_pieces[2]} = {part2_pieces[0]*part2_pieces[1]*part2_pieces[2]}")
--- 2020/tests/test_day01.py
+++ 2020/tests/test_day01.py
@@ -1,7 +1,14 @@
+from day01 import solve_part_2
 from src.day01 import solve_part_1
 
 
-def test_solve():
+def test_solve_part_1():
     parts = solve_part_1(['1721', '979', '366', '299', '675', '1456'], 2020)
     assert parts == (1721, 299)
     assert parts[0] * parts[1] == 514579
+
+
+def test_solve_part_2():
+    parts = solve_part_2(['1721', '979', '366', '299', '675', '1456'], 2020)
+    assert parts == (979, 366, 675)
+    assert parts[0] * parts[1] * parts[2] == 241861950
