f3708ea28db6ba7046c4a9f177552fcfaefdef88
Phillip Verheyden <pverheyden@gmail.com>
2020-12-06 10:27:43 -0600

    Optimize Day 6 part2 solution

--- 2020/src/day06.py
+++ 2020/src/day06.py
@@ -28,27 +28,12 @@ def part2_answer_counter(people: list[str]) -> int:
     return sum([1 for key, val in answers.items() if val == len(people)])
 
 
-def yes_answers_in_groups(lines: list[str], group_yes_counter: Callable[[list[str]], int]) -> int:
-    unique_answers = 0
-    idx = 0
-    while idx < len(lines):
-        group_data = [lines[idx]]
-        idx += 1
-        while idx < len(lines):
-            curr = lines[idx]
-            if curr == '':
-                break
-            group_data.append(curr)
-            idx += 1
-
-        unique_answers += group_yes_counter(group_data)
-        idx += 1
-
-    return unique_answers
+def yes_answers_in_groups(records: list[str], group_yes_counter: Callable[[list[str]], int]) -> int:
+    return sum([group_yes_counter(record.split()) for record in records])
 
 
 if __name__ == '__main__':
-    parsed_input = parse_to_lines('06')
+    parsed_input = parse_to_lines('06', '\n\n')
     part1 = yes_answers_in_groups(parsed_input, part1_answer_counter)
     print(f"Part 1: {part1}")
     assert part1 == 6273
