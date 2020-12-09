5d325681d85de14cb61ed657d7844d90a6c53112
Phillip Verheyden <pverheyden@gmail.com>
2020-12-08 23:19:29 -0600

    Solve day 8 part 2

--- 2020/src/day08.py
+++ 2020/src/day08.py
@@ -1,12 +1,13 @@
 from utils import parse_to_lines
 
 
-def run_program(instructions: list[str]):
+def run_program(instructions: list[str]) -> tuple[int, bool]:
     seen_instructions = set()
 
     instruction_pointer = 0
     acc = 0
-    while instruction_pointer not in seen_instructions:
+    exited_early = False
+    while not exited_early and instruction_pointer != len(instructions):
         seen_instructions.add(instruction_pointer)
         operation, value = instructions[instruction_pointer].split(' ')
         if operation == 'nop':
@@ -17,14 +18,33 @@ def run_program(instructions: list[str]):
             acc += int(value)
             instruction_pointer += 1
 
-    return acc
+        if instruction_pointer in seen_instructions or instruction_pointer > len(instructions) or instruction_pointer < 0: # protect jumping way too far
+            exited_early = True
+
+    return acc, exited_early
+
+
+def part2(instructions: list[str]) -> int:
+    for idx, instr in enumerate(instructions):
+        prev = instr
+        if instr.startswith('nop'):
+            instructions[idx] = instr.replace('nop', 'jmp')
+        elif instr.startswith('jmp'):
+            instructions[idx] = instr.replace('jmp', 'nop')
+
+        acc, exited_early = run_program(instructions)
+        if not exited_early:
+            return acc
+        else:
+            instructions[idx] = prev
 
 
 if __name__ == '__main__':
-    part1 = run_program(parse_to_lines('08'))
+    lines = parse_to_lines('08')
+    part1, _ = run_program(lines)
     print(f"Part 1: {part1}")
-    # assert part1 == 246
-    #
-    # part2 = count_contained_bags('shiny gold', ruleset)
-    # print(f"Part 2: {part2}")
-    # # assert part2 == 3254
+    assert part1 == 1915
+
+    part2 = part2(lines)
+    print(f"Part 2: {part2}")
+    assert part2 == 944
--- 2020/tests/test_day08.py
+++ 2020/tests/test_day08.py
@@ -1,4 +1,7 @@
-from day08 import run_program
+from day08 import (
+    run_program,
+    part2,
+)
 
 
 def test_sample_part1():
@@ -11,4 +14,17 @@ acc -99
 acc +1
 jmp -4
 acc +6'''.split('\n')
-    assert run_program(sample) == 5
+    assert run_program(sample) == (5, True)
+
+
+def test_sample_part2():
+    sample = '''nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6'''.split('\n')
+    assert part2(sample) == 8
